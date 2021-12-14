8f9a484f4f0101c19ae403c635f19aa1d09dc855
Sara Betancur <sarabetancurocampo22@gmail.com>
2021-12-14 18:35:38 -0500

    cliente batman

---
+++ js/llenadotienda.js
@@ -0,0 +1,29 @@
+/*let productos=["camiseta","gorra","termo"]
+console.log(productos)
+console.log(productos[0]) 
+
+let objetoProductos={
+    nombre:"camiseta",
+    precio:50000,
+    cantidad:5,
+    foto:"http://www.fotos.com"
+}
+
+console.log(objetoProductos)
+console.log(objetoProductos.nombre)*/
+
+//Maizena de arreglos con sabor a objetos
+
+let productos=[
+    {nombre:"batimovil", precio:65000, cantidad:0, foto:'img/tienda1.jpg'}, 
+    {nombre:"camiseta", precio:50000, cantidad:0, foto:'img/tienda2.jpg'},
+    {nombre:"gorra", precio:25000, cantidad:0, foto:"'img/tienda3.jpg'}, 
+    {nombre:"termo", precio:15000, cantidad:0, foto:'img/tienda4.jpg'},
+    {nombre:"juguete", precio:50000, cantidad:0, foto:'img/tienda5.jpg'},
+    {nombre:"morral", precio:75000, cantidad:0, foto:'img/tienda6.jpg'},
+    {nombre:"silla", precio:250000, cantidad:0, foto:'img/tienda7.jpg'},
+    {nombre:"ordenador",precio:30000,cantidad:0,foto:'img/tienda8.jpg'},
+    {nombre:"caja",precio:28000,cantidad:0,foto:'img/tienda9.jpg'},
+    {nombre:"riñonera",precio:10000,cantidad:0,foto:'img/tienda10.jpg'},
+]
+
